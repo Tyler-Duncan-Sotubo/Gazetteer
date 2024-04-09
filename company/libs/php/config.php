f5581e575f39e247483529df8719de3f9e93bcf1
Tyler Duncan Sotubo <80166125+Tyler-Duncan-Sotubo@users.noreply.github.com>
2024-04-09 14:01:24 +0100

    Delete company directory

--- company/libs/php/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$servername = "127.0.0.1"; // Change this to your database server hostname
-$username = "root";     // Change this to your database username
-$password = "";     // Change this to your database password
-$database = "companydirectory";     // Change this to your database name
